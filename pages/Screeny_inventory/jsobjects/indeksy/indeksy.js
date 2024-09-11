4f0c62e590dc555b405fdbc69674e622d5770df4
Artur Zimoń <zimart@wp.pl>
2024-09-11 22:41:29 +0000

    nie dziala indeks wys

--- pages/Screeny_inventory/jsobjects/indeksy/indeksy.js
+++ pages/Screeny_inventory/jsobjects/indeksy/indeksy.js
@@ -1,6 +1,6 @@
 export default {
   indeksszer: function (szerokosc) {
-    var szerokoscm= szerokosc_input.text
+    var szerokoscm= szerokosc_input.text;
 		
 		if (szerokoscm > 0 && szerokoscm <=1000) {var szerm = 1000;}	
 		else if (szerokoscm > 1000 && szerokoscm <=1250) {var szerm = 1250 ;}
@@ -14,23 +14,19 @@ export default {
   },
 	
 	 indekswys: function (wysokosc) {
-    var szerokoscm =wysokosc_input.text
-		if (szerokoscm > 0 && szerokoscm <=1000) {var szerm = 1000;}	
-		else if (szerokoscm > 1000 && szerokoscm <=1250) {var szerm = 1250 ;}
-		else if (szerokoscm > 1250 && szerokoscm <=1500) {var szerm = 1500 ;}
-		else if (szerokoscm > 1500 && szerokoscm <=1750) {var szerm = 1750 ;}
-		else if (szerokoscm > 1750 && szerokoscm <=2000) {var szerm = 2000 ;}
-		else if (szerokoscm > 2000 && szerokoscm <=2250) {var szerm = 2250 ;}
-		else if (szerokoscm > 2250 && szerokoscm <=2500) {var szerm = 2500 ;}
-		else if (szerokoscm > 2500 && szerokoscm <=2750) {var szerm = 2750 ;}
-		return szerm;
+    var szerokoscm2 = appsmith.store.calkowita2;
+		 var szerm55
+		if (szerokoscm2 > 0 && szerokoscm2 <=1000) {var szerm55 = 1000;}	
+		else if (szerokoscm2 > 1000 && szerokoscm2 <=1250) {var szerm55 = 1250 ;}
+		else if (szerokoscm2 > 1250 && szerokoscm2 <=1500) {var szerm55 = 1500 ;}
+		else if (szerokoscm2 > 1500 && szerokoscm2 <=1750) {var szerm55 = 1750 ;}
+		else if (szerokoscm2 > 1750 && szerokoscm2 <=2000) {var szerm55 = 2000 ;}
+		else if (szerokoscm2 > 2000 && szerokoscm2 <=2250) {var szerm55 = 2250 ;}
+		else if (szerokoscm2 > 2250 && szerokoscm2 <=2500) {var szerm55 = 2500 ;}
+		else if (szerokoscm2 > 2500 && szerokoscm2 <=2750) {var szerm55 = 2750 ;}
+		return szerm55;
   },
 	
-	indekssku: function () {
-    var szerokoscm = wysokosc
-		var wysokosc = wysokosc
-				
-		return szermokoscm;
-  },
+	
 	
 };
