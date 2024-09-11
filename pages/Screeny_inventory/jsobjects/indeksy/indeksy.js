8a14a42184023cfa8af8d94f27fb91898cccfe6a
Artur Zimoń <zimart@wp.pl>
2024-09-11 15:57:31 +0000

    cena_dziala

---
+++ pages/Screeny_inventory/jsobjects/indeksy/indeksy.js
@@ -0,0 +1,36 @@
+export default {
+  indeksszer: function (szerokosc) {
+    var szerokoscm= szerokosc_input.text
+		
+		if (szerokoscm > 0 && szerokoscm <=1000) {var szerm = 1000;}	
+		else if (szerokoscm > 1000 && szerokoscm <=1250) {var szerm = 1250 ;}
+		else if (szerokoscm > 1250 && szerokoscm <=1500) {var szerm = 1500 ;}
+		else if (szerokoscm > 1500 && szerokoscm <=1750) {var szerm = 1750 ;}
+		else if (szerokoscm > 1750 && szerokoscm <=2000) {var szerm = 2000 ;}
+		else if (szerokoscm > 2000 && szerokoscm <=2250) {var szerm = 2250 ;}
+		else if (szerokoscm > 2250 && szerokoscm <=2500) {var szerm = 2500 ;}
+		else if (szerokoscm > 2500 && szerokoscm <=2750) {var szerm = 2750 ;}
+		return szerm;
+  },
+	
+	 indekswys: function (wysokosc) {
+    var szerokoscm =wysokosc_input.text
+		if (szerokoscm > 0 && szerokoscm <=1000) {var szerm = 1000;}	
+		else if (szerokoscm > 1000 && szerokoscm <=1250) {var szerm = 1250 ;}
+		else if (szerokoscm > 1250 && szerokoscm <=1500) {var szerm = 1500 ;}
+		else if (szerokoscm > 1500 && szerokoscm <=1750) {var szerm = 1750 ;}
+		else if (szerokoscm > 1750 && szerokoscm <=2000) {var szerm = 2000 ;}
+		else if (szerokoscm > 2000 && szerokoscm <=2250) {var szerm = 2250 ;}
+		else if (szerokoscm > 2250 && szerokoscm <=2500) {var szerm = 2500 ;}
+		else if (szerokoscm > 2500 && szerokoscm <=2750) {var szerm = 2750 ;}
+		return szerm;
+  },
+	
+	indekssku: function () {
+    var szerokoscm = wysokosc
+		var wysokosc = wysokosc
+				
+		return szermokoscm;
+  },
+	
+};
