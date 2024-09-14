731f85c98e1fbcefe21be4e9f730b6e9c6176bd0
Artur Zimoń <zimart@wp.pl>
2024-09-14 15:11:58 +0000

    PRZED USUNIECIEM CENY ZAPYTAJ

--- pages/testpdf/jsobjects/html2/html2.js
+++ pages/testpdf/jsobjects/html2/html2.js
@@ -1,12 +1,13 @@
 export default {
 generateHTML(data) {
-let html = Name Age ;
+let html = ID Rodzaj Tkaniny 
+; // Start table and add headers
 data.forEach(row => {
-html += ${row.name} ${row.age} 
-;
+html += ${row.id} ${row.rodzaj_tkaniny} 
+; // Add rows with data
 });
 html += 
-;
+; // Close table
 return html;
 }
 };
